e6afd9d28c1e419d9a216cdef5adaacddad091a0
salah204087 <salah204087@gmail.com>
2023-07-22 18:44:56 +0300

    Making the DTO for the GameType

---
+++ Game_StoreAPI/Models/DTO/GameTypeUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Game_StoreAPI.Models.DTO
+{
+    public class GameTypeUpdateDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string? Name { get; set; }
+        public string? About { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
